583672e79c86af1d6ea2859b644f593e29fb2f92
Yusuf <yusuf24ef@gmail.com>
2026-04-14 23:35:09 +0700

    Standardize login page browser title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ \Illuminate\Support\Str::title(\App\Models\Setting::get('store_name') ?? config('app.name', 'Apotek')) }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
